cdd2523a34675c812838ae270226d4f7178524e0
Tarrots <thanhphong.tarrots@gmail.com>
2021-08-18 12:45:28 +0700

    add Java

---
+++ Java/Servlet_Jsp_MVC/src/main/java/com/servletjsp/model/CommentsModel.java
@@ -0,0 +1,5 @@
+package com.servletjsp.model;
+
+public class CommentsModel {
+
+}
---
+++ Java/Servlet_Jsp_MVC/src/main/java/com/servletjsp/model/NewsModel.java
@@ -0,0 +1,16 @@
+package com.servletjsp.model;
+
+import java.sql.Timestamp;
+
+public class NewsModel {
+	private Long id;
+	private String title;
+	private String thumbNail;
+	private Long categoryId;
+	private Timestamp createdDate;
+	private Timestamp modifiedDate;
+	private String createdBy;
+	private String modifiedBy;
+	
+	
+}
